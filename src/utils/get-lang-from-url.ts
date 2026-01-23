d507dff8d46a25acfbf076796c898fc2533d9580
Matheus Mota <matheussilvamartins1714@gmail.com>
2026-01-22 23:18:42 -0300

    refactor: remove i18n support and translations, update components to use static text

--- src/utils/get-lang-from-url.ts
+++
@@ -1,7 +0,0 @@
-import { defaultLang, languages } from "@/hooks/use-translations";
-
-export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
-  if (languages.includes(lang)) return lang;
-  return defaultLang;
-}
